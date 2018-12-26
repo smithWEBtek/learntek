e4570d2b3aa51202679f13c3a76dbd1d04e59810
smithwebtek <brad@smithwebtek.com>
2018-12-25 22:46:53 -0500

    Add cloudinary.yml

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -5,8 +5,6 @@ class Resource < ApplicationRecord
 
 	def self.getCloudinaryResources
 		result = {}
-		Resource.all.each{|r|r.delete}
-		ActiveRecord::Base.connection.reset_pk_sequence!('resources')
 		response = Cloudinary::Api.resources(resource: 'image', format: 'pdf', max_results: 500)		
 		response['resources'].each do |resource|
 			if !Resource.find_by_public_id(resource["public_id"])
