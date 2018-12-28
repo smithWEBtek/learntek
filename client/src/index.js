72e02d1d47181bbce78d71f9a4ae94c016c1182e
smithwebtek <brad@smithwebtek.com>
2018-12-28 13:34:01 -0500

    Get spinner working, refactor more js to jquery

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,9 @@ $(() => {
 })
 
 // figure out how to have a fallback api source for Ajax request
-// const baseUrl = 'https://learntek.herokuapp.com/api/'
+const baseUrl = 'https://learntek.herokuapp.com/api/'
 // const baseUrl = 'http://localhost:3000/api/' || 'https://learntek.herokuapp.com/api/'
-const baseUrl = 'http://localhost:3000/api/'
+// const baseUrl = 'http://localhost:3000/api/'
 
 function clearApiDataDiv() {
 	$('div#api-data').html('')
@@ -27,58 +27,55 @@ function getApiData(event) {
 	event.preventDefault()
 	let url = this.id
 	let dataDiv = $('div#api-data')
+	dataDiv.html(`<p><i class="fa fa-spinner fa-spin" style="font-size:24px"></i>    fetching <strong>${url}</strong> API data</p>`)
 
+	setTimeout(() => {
+		$.ajax({
+			url: baseUrl + url,
+			method: 'get',
+			dataType: 'json'
+		}).done(function (data) {
+			clearApiDataDiv()
+			clearNewFormDiv()
+			switch (url) {
+				case 'tracks':
+					data.forEach(item => {
+						let newTrack = new Track(item)
+						dataDiv.append(newTrack.trackHTML())
+					})
+					break;
 
-	dataDiv.html('...loading...')
+				case 'resources':
+					data.forEach(item => {
+						let newResource = new Resource(item)
+						dataDiv.append(newResource.resourceHTML())
+					})
+					break;
 
-	$.ajax({
-		url: baseUrl + url,
-		method: 'get',
-		dataType: 'json'
-	}).done(function (data) {
-		clearApiDataDiv()
-		clearNewFormDiv()
-		switch (url) {
-			case 'tracks':
-				data.forEach(item => {
-					let newTrack = new Track(item)
-					dataDiv.append(newTrack.trackHTML())
-				})
-				break;
+				case 'activities':
+					data.forEach(item => {
+						let newActivity = new Activity(item)
+						dataDiv.append(newActivity.activityHTML())
+					})
+					break;
 
-			case 'resources':
-				data.forEach(item => {
-					let newResource = new Resource(item)
-					dataDiv.append(newResource.resourceHTML())
-				})
-				break;
+				case 'categories':
+					data.forEach(item => {
+						let newCategory = new Category(item)
+						dataDiv.append(newCategory.categoryHTML())
+					})
+					listenCategoryResources()
+					break;
 
-			case 'activities':
-				data.forEach(item => {
-					let newActivity = new Activity(item)
-					dataDiv.append(newActivity.activityHTML())
-				})
-				break;
-
-			case 'categories':
-				data.forEach(item => {
-					let newCategory = new Category(item)
-					dataDiv.append(newCategory.categoryHTML())
-				})
-				listenCategoryResources()
-				break;
-
-			default:
-				console.log('there was no data returned');
-		}
-	})
+				default:
+					console.log('there was no data returned');
+			}
+		})
+	}, 2500);
 }
 
 function listenForNewFormLinks() {
-	let apiNewLinks = document.querySelectorAll('tag.api-new-links')
-	apiNewLinks.forEach(link => {
-		link.addEventListener('click', getNewForm)
-	})
+	$('tag.api-new-links').on('click', getNewForm)
 }
 
 function getNewForm(event) {
